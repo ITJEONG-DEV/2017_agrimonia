b2e43be791b3e5b92e5f3254e0f19d5c8490fdae
jinmuk128 <jinmuk128@naver.com>
2017-08-03 00:04:55 +0900

    어너...

--- ægrimónĭa/sound/sound.lua
+++ ægrimónĭa/sound/sound.lua
@@ -2,10 +2,10 @@ local M = {}
 
 M.bar = audio.loadSound("sound/Let's talk a bit.ogg")
 M.bell = audio.loadSound("sound/bell.wav")
-M.schi = audio.loadSound("Schizophrenia.mp3")
+M.schi = audio.loadSound("sound/Schizophrenia.mp3")
 M.defend = audio.loadSound("sound/defendGood.wav")
 M.hurt = audio.loadSound("sound/ImTooHurt.wav")
-M.Qdeal = audio.loadSound("Qdeal.wav")
-M.Edeal = audio.loadSound("Edeal.wav")
+M.Qdeal = audio.loadSound("sound/Qdeal.wav")
+M.Edeal = audio.loadSound("sound/Edeal.wav")
 
 return M
