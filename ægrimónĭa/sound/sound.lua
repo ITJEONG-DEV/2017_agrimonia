f3148796917ea0c2f234c71d711be61a8af729c5
jinmuk128 <jinmuk128@naver.com>
2017-08-02 23:55:08 +0900

    아니 뭐 어디가 에러가 난건데...

--- ægrimónĭa/sound/sound.lua
+++ ægrimónĭa/sound/sound.lua
@@ -1,12 +1,11 @@
 local M = {}
 
 M.bar = audio.loadSound("sound/Let's talk a bit.ogg")
-M.bell = audio.loadSound( "sound/bell.wav")
-M.schi = audio.loadSound("sound/Schizophrenia.mp3")
+M.bell = audio.loadSound("sound/bell.wav")
+M.schi = audio.loadSound("Schizophrenia.mp3")
 M.defend = audio.loadSound("sound/defendGood.wav")
 M.hurt = audio.loadSound("sound/ImTooHurt.wav")
---M.Qdeal = audio.loadSound("sound/Qdeal.wav")
-M.Edeal = audio.loadSound("sound/Edeal.wav")
-M.start = audio.loadSound("sound/start.mp3")
+M.Qdeal = audio.loadSound("Qdeal.wav")
+M.Edeal = audio.loadSound("Edeal.wav")
 
 return M
