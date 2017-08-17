7ad53a31c8d4b1243d1d2a506fec10262fc4afc9
ITJEONG-NAN-JJANG <derbana1027@gmail.com>
2017-08-17 09:02:37 +0900

    a
    a

--- ægrimónĭa/sound/sound.lua
+++ ægrimónĭa/sound/sound.lua
@@ -5,11 +5,12 @@ M.bell = audio.loadSound("sound/bell.wav")
 M.schi = audio.loadSound("sound/Schizophrenia.mp3")
 M.defend = audio.loadSound("sound/defendGood.wav")
 M.hurt = audio.loadSound("sound/ImTooHurt.wav")
-M.Qdeal = audio.loadSound("sound/Qdeal.wav")
+--M.Qdeal = audio.loadSound("sound/Qdeal.wav")
 M.Edeal = audio.loadSound("sound/Edeal.wav")
 M.bell = audio.loadSound("sound/bell.wav")
 M.bar = audio.loadSound("sound/Let's talk a bit.ogg")
 M.start = audio.loadSound("sound/start.mp3")
 M.arena = audio.loadSound( "sound/amuten.mp3")
-
+M.mgsg = "sound/mgsg.wav"
+M.jgg = "sound/jgg.wav"
 return M
